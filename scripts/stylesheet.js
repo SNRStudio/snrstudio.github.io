6c31129ca4bdc844c580fe82ea9f02ed3f2cb0cd
还给我六个币 <113269187+HuanGeiWoLiuGeBi@users.noreply.github.com>
2022-10-26 19:22:24 +0800

    Add files via upload

--- scripts/stylesheet.js
+++ scripts/stylesheet.js
@@ -2,3 +2,4 @@ document.write("		<link href=\"..\/styles\/common.css\" rel=\"stylesheet\">");
 document.write("		<link href=\"..\/styles\/navbar.css\" rel=\"stylesheet\">");
 document.write("		<link href=\"..\/styles\/article.css\" rel=\"stylesheet\">");
 document.write("		<link href=\"..\/styles\/text.css\" rel=\"stylesheet\">");
+document.write("		<link href=\"..\/styles\/download.css\" rel=\"stylesheet\">");
