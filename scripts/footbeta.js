94a5d3d59a6b1e1b95a0d756ef326a59003764d4
Return761 <asdf4321hy@163.com>
2022-10-29 14:32:48 +0800

    u

--- scripts/footbeta.js
+++ scripts/footbeta.js
@@ -1,6 +1,6 @@
 document.writeln("<div class=\'footbeta\'>");
 document.writeln("  <footer class=\'footbeta-block\'>");
 document.writeln("      <p class=\'footbeta-maintext\'>SNR官方网站施工中,当前非最终效果<p>");
-document.writeln("      <p class=\'footbeta-text>@SNR WebsiteV2<p>");
+document.writeln("      <p class=\'footbeta-text\'>@SNR WebsiteV2<p>");
 document.writeln("  </footer>");
 document.writeln("</div>");
--- scripts/stylesheet.js
+++ scripts/stylesheet.js
@@ -3,3 +3,4 @@ document.write("		<link href=\"..\/styles\/navbar.css\" rel=\"stylesheet\">");
 document.write("		<link href=\"..\/styles\/article.css\" rel=\"stylesheet\">");
 document.write("		<link href=\"..\/styles\/text.css\" rel=\"stylesheet\">");
 document.write("		<link href=\"..\/styles\/download.css\" rel=\"stylesheet\">");
+document.write("		<link href=\"..\/styles\/footbeta.css\" rel=\"stylesheet\">");
