c8b952f8d67338859b41e2cc9fa0f936ee9aae1b
Return761 <asdf4321hy@163.com>
2022-10-29 14:19:38 +0800

    u

--- scripts/footbeta.js
+++ scripts/footbeta.js
@@ -1,5 +1,5 @@
 document.writeln("<div class=\'footbeta\'>");
-document.writeln("  <footer class=\'footbeta\'>");
+document.writeln("  <footer class=\'footbeta-block\'>");
 document.writeln("      <p class=\'footbeta-maintext\'>SNR官方网站施工中,当前非最终效果<p>");
 document.writeln("      <p class=\'footbeta-text>@SNR WebsiteV2<p>");
 document.writeln("  </footer>");
