de16f7ee07a870c335cf25db101c72581bfad23e
还给我六个币 <six_coin@foxmail.com>
2022-11-02 17:22:21 +0800

    Delete notinuse directory

--- notinuse/footbeta.js
+++
@@ -1,5 +0,0 @@
-document.writeln("<div class=\'footbeta\'>");
-document.writeln("  <footer class=\'footbeta-block\'>");
-document.writeln("      <p class=\'footbeta-maintext\'>SNR官方网站施工中,当前非最终效果   @SNR WebsiteV2<p>");
-document.writeln("  </footer>");
-document.writeln("</div>");
