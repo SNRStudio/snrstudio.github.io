8fbb4ca433bc1a8f96713abe8a3b8a203aa8e977
还给我六个币 <six_coin@foxmail.com>
2022-11-04 19:38:50 +0800

    Add files via upload

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,5 +1,5 @@
 document.write("        <div class=\"navbar-block\">");
-document.write("            <img class=\"navbar-image\" src=\"..\/images\/navbar-icon.gif\">");
+document.write("            <img class=\"navbar-image\" src=\"..\/images\/navbar-icon.png\">");
 document.write("            <p class=\"navbar-title\">SNR团队官方网站<\/p>");
 document.write("            <a href=\"..\/index.html\" class=\"navbar-homepage\">首页<\/a>");
 document.write("            <a href=\"#\" class=\"navbar-homepage\" onclick=\"javascript:history.back(-1);\">上页<\/a>");
@@ -10,3 +10,4 @@ document.write("			<a href=\"https:\/\/github.com\/Return761\" class=\"navbar-we
 document.write("			<a href=\"https:\/\/space.bilibili.com\/1986062921\" class=\"navbar-website\" target=\"_blank\">Bilibili<\/a>");
 document.write("			<p class=\"navbar-websitet\">Return_761<\/p>");
 document.write("        <\/div>");
+document.write("        <p class=\"article-space70px\"><\/p>");
