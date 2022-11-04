b07a1d3e772ea5185261637de58023a85294d8a2
Return761 <asdf4321hy@163.com>
2022-11-04 20:46:42 +0800

    Update Footer

--- scripts/head.js
+++ scripts/head.js
@@ -2,3 +2,4 @@ document.write("        <link href=\"..\/styles\/body.css\" rel=\"stylesheet\">"
 document.write("        <link href=\"..\/styles\/navbar.css\" rel=\"stylesheet\">");
 document.write("        <link href=\"..\/styles\/article.css\" rel=\"stylesheet\">");
 document.write("        <link href=\"..\/styles\/text.css\" rel=\"stylesheet\">");
+document.write("        <link href=\"..\/styles\/footer.css\" rel=\"stylesheet\">");
