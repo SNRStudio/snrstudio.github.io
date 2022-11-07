acf241ebc7d9964e880a0f49c0316b4a5810e0a1
还给我六个币 <six_coin@foxmail.com>
2022-11-07 20:00:22 +0800

    Add files via upload

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,8 +1,8 @@
 document.write("<div class=\'footer-block\'>");
+document.write("    <p class=\"footer-websitet\">还给我六个币<\/p>");
 document.write("    <a href =\"https:\/\/github.com\/HuanGeiWoLiuGeBi\" class=\"footer-website\" target=\"_blank\">Github<\/a>");
 document.write("    <a href=\"https:\/\/space.bilibili.com\/590621781\" class=\"footer-website\" target=\"_blank\">Bilibili<\/a>");
-document.write("    <p class=\"footer-websitet\">还给我六个币<\/p>");
+document.write("    <p class=\"footer-websitet\">Return_761<\/p>");
 document.write("    <a href=\"https:\/\/github.com\/Return761\" class=\"footer-website\" target=\"_blank\">Github<\/a>");
 document.write("    <a href=\"https:\/\/space.bilibili.com\/1986062921\" class=\"footer-website\" target=\"_blank\">Bilibili<\/a>");
-document.write("    <p class=\"footer-websitet\">Return_761<\/p>");
 document.write("</div>");
--- scripts/head.js
+++ scripts/head.js
@@ -3,3 +3,4 @@ document.write("        <link href=\"..\/styles\/navbar.css\" rel=\"stylesheet\"
 document.write("        <link href=\"..\/styles\/article.css\" rel=\"stylesheet\">");
 document.write("        <link href=\"..\/styles\/text.css\" rel=\"stylesheet\">");
 document.write("        <link href=\"..\/styles\/footer.css\" rel=\"stylesheet\">");
+document.write("        <link rel=\"shortcut icon\" href=\"..\/images\/website-icon.png\">");
