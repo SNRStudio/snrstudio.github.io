eee1fbbd22f005add0c83b63f1405c2a820216fc
Return761 <asdf4321hy@163.com>
2022-11-09 08:31:41 +0800

    U

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,6 +2,8 @@ document.write("        <div class=\"navbar-block\">");
 document.write("            <img class=\"navbar-image\" src=\"..\/images\/navbar-icon.png\">");
 document.write("            <p class=\"navbar-title\">SNR团队官方网站<\/p>");
 document.write("            <a href=\"..\/index.html\" class=\"navbar-homepage\">首页<\/a>");
+document.write("            <a href=\"..\/pages/B-1-minecraft.html\" class=\"navbar-homepage\">Minecraft版<\/a>");
+document.write("            <a href=\"..\/pages/B-allarticles.html\" class=\"navbar-homepage\">所有文章<\/a>");
 document.write("            <a href=\"..\/pages/A-infopage.html\" class=\"navbar-homepage\">关于<\/a>");
 document.write("            <a href=\"#\" class=\"navbar-homepage\" onclick=\"javascript:history.back(-1);\">上页<\/a>");
 document.write("        <\/div>");
