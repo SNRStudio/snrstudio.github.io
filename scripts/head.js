0162b33420de73d742e37052f34df2c1be513f9b
还给我六个币 <six_coin@foxmail.com>
2022-11-11 21:25:52 +0800

    Delete scripts directory

--- scripts/head.js
+++
@@ -1,6 +0,0 @@
-document.write("        <link href=\"..\/styles\/body.css\" rel=\"stylesheet\">");
-document.write("        <link href=\"..\/styles\/navbar.css\" rel=\"stylesheet\">");
-document.write("        <link href=\"..\/styles\/article.css\" rel=\"stylesheet\">");
-document.write("        <link href=\"..\/styles\/text.css\" rel=\"stylesheet\">");
-document.write("        <link href=\"..\/styles\/footer.css\" rel=\"stylesheet\">");
-document.write("        <link rel=\"shortcut icon\" href=\"..\/images\/website-icon.png\">");
