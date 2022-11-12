c647d0dd00df59dcdc6aa575b8db3399c4bf3d15
还给我六个币 <six_coin@foxmail.com>
2022-11-12 10:29:58 +0800

    Add files via upload

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,4 +1,4 @@
-document.write("<div class=\'article-block\' style=\'padding: 0px; height: 48px;\'>");
+document.write("<div class=\'article-block\' style=\'padding: 0px; height: 50px; overflow: hidden;\'>");
 document.write("    <p class=\"text-footer-websitetitle\">还给我六个币<\/p>");
 document.write("    <a href =\"https:\/\/github.com\/HuanGeiWoLiuGeBi\" class=\"text-footer-website\" target=\"_blank\">Github<\/a>");
 document.write("    <a href=\"https:\/\/space.bilibili.com\/590621781\" class=\"text-footer-website\" target=\"_blank\">Bilibili<\/a>");
