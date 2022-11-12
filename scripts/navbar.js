5c6fbf0935d495a49e4320bdbf5d2dd27fce6a14
还给我六个币 <six_coin@foxmail.com>
2022-11-12 14:06:56 +0800

    Add files via upload

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,9 +1,9 @@
 document.write("        <div class=\"navbar-block\">");
-document.write("            <img class=\"navbar-image\" src=\"..\/images\/navbar-icon.png\">");
+document.write("            <img class=\"navbar-image\" src=\"..\/images\/navbar-icon.png\" style=\"height: 40px; width: 35px;\">");
 document.write("            <p class=\"navbar-title\">SNR团队官方网站<\/p>");
 document.write("            <a href=\"..\/index.html\" class=\"navbar-homepage\">首页<\/a>");
 document.write("            <a href=\"#\" class=\"navbar-homepage\" onclick=\"javascript:history.back(-1);\">上页<\/a>");
-document.write("            <a href=\"..\/pages/A-infopage.html\" class=\"navbar-homepage\">关于<\/a>");
+document.write("            <a href=\"..\/pages/A-infopage.html\" class=\"navbar-homepage\">关于本站<\/a>");
 document.write("            <a href=\"..\/pages/B-1-allarticles.html\" class=\"navbar-homepage\">所有文章<\/a>");
 document.write("        <\/div>");
 document.write("        <p class=\"article-space70px\"><\/p>");
