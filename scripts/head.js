9141d29aa4d53a7d91ae3d1fe719352f577d5827
还给我六个币 <six_coin@foxmail.com>
2022-11-12 16:20:52 +0800

    Add files via upload

--- scripts/head.js
+++ scripts/head.js
@@ -1,6 +1,7 @@
-document.write("        <link href=\"..\/styles\/body.css\" rel=\"stylesheet\">");
+document.write("        <link href=\"..\/styles\/attribute.css\" rel=\"stylesheet\">");
+document.write("        <link href=\"..\/styles\/block.css\" rel=\"stylesheet\">");
+document.write("        <link href=\"..\/styles\/element.css\" rel=\"stylesheet\">");
 document.write("        <link href=\"..\/styles\/navbar.css\" rel=\"stylesheet\">");
-document.write("        <link href=\"..\/styles\/article.css\" rel=\"stylesheet\">");
+document.write("        <link href=\"..\/styles\/space.css\" rel=\"stylesheet\">");
 document.write("        <link href=\"..\/styles\/text.css\" rel=\"stylesheet\">");
-document.write("        <link href=\"..\/styles\/image.css\" rel=\"stylesheet\">");
-document.write("        <link rel=\"shortcut icon\" href=\"..\/images\/website-icon.png\">");
+document.write("        <link rel=\"shortcut icon\" href=\"..\/images\/wicon.png\">");
