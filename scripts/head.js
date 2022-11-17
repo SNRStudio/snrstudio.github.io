3ccea2e6e49b7258ba61cf73c29c82acabc9defd
还给我六个币 <six_coin@foxmail.com>
2022-11-17 17:30:20 +0800

    Add files via upload

--- scripts/head.js
+++ scripts/head.js
@@ -1,7 +1,7 @@
 document.write("        <link href=\"..\/styles\/attribute.css\" rel=\"stylesheet\">");
-document.write("        <link href=\"..\/styles\/block.css\" rel=\"stylesheet\">");
 document.write("        <link href=\"..\/styles\/element.css\" rel=\"stylesheet\">");
-document.write("        <link href=\"..\/styles\/navbar.css\" rel=\"stylesheet\">");
-document.write("        <link href=\"..\/styles\/space.css\" rel=\"stylesheet\">");
+document.write("        <link href=\"..\/styles\/block.css\" rel=\"stylesheet\">");
 document.write("        <link href=\"..\/styles\/text.css\" rel=\"stylesheet\">");
+document.write("        <link href=\"..\/styles\/space.css\" rel=\"stylesheet\">");
+document.write("        <link href=\"..\/styles\/navbar.css\" rel=\"stylesheet\">");
 document.write("        <link rel=\"shortcut icon\" href=\"..\/images\/wicon.png\">");
