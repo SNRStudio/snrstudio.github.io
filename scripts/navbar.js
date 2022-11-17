c9ab4ae0eb6f115ccb7db0f0413263b8bda1b244
还给我六个币 <six_coin@foxmail.com>
2022-11-17 18:05:54 +0800

    Add files via upload

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,10 +1,10 @@
 document.write("        <div class=\"b-navbar\">");
 document.write("            <img class=\"n-image\" src=\"..\/images\/nicon.png\">");
 document.write("            <p class=\"t-navbartitle\">SNR团队官方网站<\/p>");
-document.write("            <a href=\"..\/index.html\" class=\"t-navbarlink\">首页<\/a>");
+document.write("            <a href=\"A-1.html\" class=\"t-navbarlink\">首页<\/a>");
 document.write("            <a href=\"#\" class=\"t-navbarlink\" onclick=\"javascript:history.back(-1);\">上页<\/a>");
-document.write("            <a href=\"..\/pages/A-3.html\" class=\"t-navbarlink\">关于本站<\/a>");
-document.write("            <a href=\"..\/pages/B-1.html\" class=\"t-navbarlink\">所有文章<\/a>");
-document.write("            <a href=\"..\/pages/A-4.html\" class=\"t-navbarlink\">网页映射<\/a>");
+document.write("            <a href=\"A-2.html\" class=\"t-navbarlink\">关于本站<\/a>");
+document.write("            <a href=\"B-1.html\" class=\"t-navbarlink\">所有文章<\/a>");
+document.write("            <a href=\"A-3.html\" class=\"t-navbarlink\">网页映射<\/a>");
 document.write("        <\/div>");
 document.write("        <p class=\"s-70\"><\/p>");
