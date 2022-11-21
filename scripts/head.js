199ae24e49978dd02592fb8b01e99f4868f2881a
一 minormaker761 <asdf4321hy@163.com>
2022-11-21 09:11:02 +0800

    Dark Mode update!!!

--- scripts/head.js
+++ scripts/head.js
@@ -1,7 +1,19 @@
-document.write("        <link href=\"..\/styles\/attribute.css\" rel=\"stylesheet\">");
-document.write("        <link href=\"..\/styles\/element.css\" rel=\"stylesheet\">");
-document.write("        <link href=\"..\/styles\/block.css\" rel=\"stylesheet\">");
-document.write("        <link href=\"..\/styles\/text.css\" rel=\"stylesheet\">");
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+    document.write("        <link href=\"..\/styles\/Light-Style\/light-attribute.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/Light-Style\/light-element.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/Light-Style\/light-block.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/Light-Style\/light-text.css\" rel=\"stylesheet\">");
+} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.write("        <link href=\"..\/styles\/Dark-Style\/dark-attribute.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/Dark-Style\/dark-element.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/Dark-Style\/dark-block.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/Dark-Style\/dark-text.css\" rel=\"stylesheet\">");
+} else {
+    document.write("        <link href=\"..\/styles\/Light-Style\/light-attribute.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/Light-Style\/light-element.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/Light-Style\/light-block.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/Light-Style\/light-text.css\" rel=\"stylesheet\">");
+}
+document.write("        <link rel=\"shortcut icon\" href=\"..\/images\/wicon.png\">");
 document.write("        <link href=\"..\/styles\/space.css\" rel=\"stylesheet\">");
 document.write("        <link href=\"..\/styles\/navbar.css\" rel=\"stylesheet\">");
-document.write("        <link rel=\"shortcut icon\" href=\"..\/images\/wicon.png\">");
