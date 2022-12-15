40e846410d32796fa7d41505512df06910ac3f82
还给我六个币 <six_coin@foxmail.com>
2022-12-15 12:22:54 +0800

    Add files via upload

---
+++ scripts/A-1/1.js
@@ -0,0 +1,27 @@
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.writeln("            <div class=\"b-section\" style=\"background-image: url(..\/images\/A-1\/D1.png);\" onclick=\"window.open('..\/pages\/B-2.html', '_self')\">");
+    document.writeln("                <p class=\"s-165\"><\/p>");
+    document.writeln("                <h2 class=\"t-layout\">Minecraft版<\/h2>");
+    document.writeln("            <\/div>");
+    document.writeln("            <div class=\"b-section\" style=\"background-image: url(..\/images\/A-1\/D2.png);\" onclick=\"window.open('..\/pages\/B-3.html', '_self')\">");
+    document.writeln("                <p class=\"s-165\"><\/p>");
+    document.writeln("                <h2 class=\"t-layout\">软件推荐版<\/h2>");
+    document.writeln("            <\/div>");
+    document.writeln("            <div class=\"b-section\" style=\"background-image: url(..\/images\/A-1\/D3.png);\" onclick=\"window.open('..\/pages\/B-4.html', '_self')\">");
+    document.writeln("                <p class=\"s-165\"><\/p>");
+    document.writeln("                <h2 class=\"t-layout\">Windows版<\/h2>");
+    document.writeln("            <\/div>");
+} else {
+    document.writeln("            <div class=\"b-section\" style=\"background-image: url(..\/images\/A-1\/L1.png);\" onclick=\"window.open('..\/pages\/B-2.html', '_self')\">");
+    document.writeln("                <p class=\"s-165\"><\/p>");
+    document.writeln("                <h2 class=\"t-layout\">Minecraft版<\/h2>");
+    document.writeln("            <\/div>");
+    document.writeln("            <div class=\"b-section\" style=\"background-image: url(..\/images\/A-1\/L2.png);\" onclick=\"window.open('..\/pages\/B-3.html', '_self')\">");
+    document.writeln("                <p class=\"s-165\"><\/p>");
+    document.writeln("                <h2 class=\"t-layout\">软件推荐版<\/h2>");
+    document.writeln("            <\/div>");
+    document.writeln("            <div class=\"b-section\" style=\"background-image: url(..\/images\/A-1\/L3.png);\" onclick=\"window.open('..\/pages\/B-4.html', '_self')\">");
+    document.writeln("                <p class=\"s-165\"><\/p>");
+    document.writeln("                <h2 class=\"t-layout\">Windows版<\/h2>");
+    document.writeln("            <\/div>");
+}
--- scripts/head.js
+++ scripts/head.js
@@ -1,19 +1,16 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
-    document.write("        <link href=\"..\/styles\/Light-Style\/light-attribute.css\" rel=\"stylesheet\">");
-    document.write("        <link href=\"..\/styles\/Light-Style\/light-element.css\" rel=\"stylesheet\">");
-    document.write("        <link href=\"..\/styles\/Light-Style\/light-block.css\" rel=\"stylesheet\">");
-    document.write("        <link href=\"..\/styles\/Light-Style\/light-text.css\" rel=\"stylesheet\">");
-} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.write("        <link href=\"..\/styles\/Dark-Style\/dark-attribute.css\" rel=\"stylesheet\">");
-    document.write("        <link href=\"..\/styles\/Dark-Style\/dark-element.css\" rel=\"stylesheet\">");
-    document.write("        <link href=\"..\/styles\/Dark-Style\/dark-block.css\" rel=\"stylesheet\">");
-    document.write("        <link href=\"..\/styles\/Dark-Style\/dark-text.css\" rel=\"stylesheet\">");
-} else {
-    document.write("        <link href=\"..\/styles\/Light-Style\/light-attribute.css\" rel=\"stylesheet\">");
-    document.write("        <link href=\"..\/styles\/Light-Style\/light-element.css\" rel=\"stylesheet\">");
-    document.write("        <link href=\"..\/styles\/Light-Style\/light-block.css\" rel=\"stylesheet\">");
-    document.write("        <link href=\"..\/styles\/Light-Style\/light-text.css\" rel=\"stylesheet\">");
-}
 document.write("        <link rel=\"shortcut icon\" href=\"..\/images\/wicon.png\">");
-document.write("        <link href=\"..\/styles\/space.css\" rel=\"stylesheet\">");
-document.write("        <link href=\"..\/styles\/navbar.css\" rel=\"stylesheet\">");
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.write("        <link href=\"..\/styles\/dark\/attribute.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/dark\/element.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/dark\/block.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/dark\/text.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/dark\/space.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/dark\/navbar.css\" rel=\"stylesheet\">");
+} else {
+    document.write("        <link href=\"..\/styles\/light\/attribute.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/light\/element.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/light\/block.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/light\/text.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/light\/space.css\" rel=\"stylesheet\">");
+    document.write("        <link href=\"..\/styles\/light\/navbar.css\" rel=\"stylesheet\">");
+}
