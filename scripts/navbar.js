1d8d84018675f91a318a5f8a237effb93bd6832a
还给我六个币 <six_coin@foxmail.com>
2022-12-26 14:13:33 +0800

    Add files via upload

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -4,6 +4,6 @@ document.write("            <p class=\"t-navbartitle\">SNR团队官方网站<\/p
 document.write("            <a href=\"A-1.html\" class=\"t-navbarlink\">首页<\/a>");
 document.write("            <a href=\"#\" class=\"t-navbarlink\" onclick=\"javascript:history.back(-1);\">上页<\/a>");
 document.write("            <a href=\"B-1.html\" class=\"t-navbarlink\">所有文章<\/a>");
-document.write("            <a href=\"..\/special\/snrstartpage\/pages\/startpage.html\" class=\"t-navbarlink\">起始页<\/a>");
+document.write("            <a href=\"..\/special\/snrstartpage\/pages\/A-1.html\" class=\"t-navbarlink\">起始页<\/a>");
 document.write("        <\/div>");
 document.write("        <p class=\"s-70\"><\/p>");
