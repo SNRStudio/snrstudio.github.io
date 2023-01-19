d19ede58fc8805a883d1744ad41a6a270d23682a
一 minormaker761 <asdf4321hy@163.com>
2023-01-19 17:11:02 +0800

    增加specialprogram模板； 开新坑（指建立新文件夹）

---
+++ samples/sample-special/scripts/head.js
@@ -0,0 +1,12 @@
+document.write("        <link rel=\"shortcut icon\" href=\"..\/images\/wicon.png\">");
+document.write("        <link href=\"..\/..\/..\/styles\/attribute.css\" rel=\"stylesheet\">");
+document.write("        <link href=\"..\/styles\/element.css\" rel=\"stylesheet\">");
+document.write("        <link href=\"..\/..\/..\/styles\/element.css\" rel=\"stylesheet\">");
+document.write("        <link href=\"..\/..\/..\/styles\/block.css\" rel=\"stylesheet\">");
+document.write("        <link href=\"..\/..\/..\/styles\/text.css\" rel=\"stylesheet\">");
+document.write("        <link href=\"..\/..\/..\/styles\/space.css\" rel=\"stylesheet\">");
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.write("        <link href=\"..\/..\/..\/styles\/-dark.css\" rel=\"stylesheet\">");
+} else {
+    document.write("        <link href=\"..\/..\/..\/styles\/-light.css\" rel=\"stylesheet\">");
+}
